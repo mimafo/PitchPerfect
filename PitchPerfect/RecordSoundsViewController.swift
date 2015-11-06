b9e0ee2251b6c4e7903a044ae7073e0d938b9447
Mike F <mikefolcher@gmail.com>
2015-11-05 22:58:44 -0500

    Add pause and resume functionality to the recorder

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -9,18 +9,26 @@
 import UIKit
 import AVFoundation
 
+enum RecordingState {
+    case Initial, Recording, Record, Pausing, Paused, Stopping, Stopped
+}
+
 class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
 
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var resumeButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     
-    let RecordingString = "Recording..."
+    let RecordingString = "Recording"
     let TapInstructionString = "Tap to Record"
+    let PauseString = "Paused"
     
     //Declared Globally
     var audioRecorder:AVAudioRecorder!
     var recordedAudio:RecordedAudio!
+    var state:RecordingState = RecordingState.Initial
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,9 +37,7 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     }
     
     override func viewWillAppear(animated: Bool) {
-        self.stopButton.hidden = true;
-        self.recordingLabel.text = self.TapInstructionString
-        self.recordButton.enabled = true
+        self.changeState(RecordingState.Initial)
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,10 +46,34 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     }
 
 
+    @IBAction func pausePressed(sender: UIButton) {
+        
+        //First, update UI to pausing state
+        self.changeState(RecordingState.Pausing)
+        
+        //Pause the recording
+        self.audioRecorder.pause();
+        
+        //Set the state to the paused state
+        self.changeState(RecordingState.Paused)
+        
+    }
+    
+    @IBAction func resumePressed(sender: UIButton) {
+        //Disable the recording button first to prevent double tapping
+        self.changeState(RecordingState.Recording)
+        
+        //Now lets start recording sounds
+        self.audioRecorder.record()
+        
+        //Once recording has started, change the state of the UI
+        self.changeState(RecordingState.Record)
+    }
+    
     @IBAction func stopPressed(sender: UIButton) {
 
         //First, hide the stop button to prevent double tapping
-        self.stopButton.hidden = true
+        self.changeState(RecordingState.Stopping)
         
         //Now, Stop the audio recording
         audioRecorder.stop()
@@ -57,14 +87,13 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     @IBAction func recordAudioPressed(sender: UIButton) {
         
         //Disable the recording button first to prevent double tapping
-        self.recordButton.enabled = false
+        self.changeState(RecordingState.Recording)
         
         //Now lets start recording sounds
         self.recordSounds()
 
-        //Once recording has started, display the recording string and stop button
-        self.recordingLabel.text = self.RecordingString
-        self.stopButton.hidden = false
+        //Once recording has started, change the state of the UI
+        self.changeState(RecordingState.Record)
     
         print("In recordAudioPressed")
         
@@ -108,12 +137,54 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         let session = AVAudioSession.sharedInstance()
         try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
         
-        try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
-        audioRecorder.delegate = self
-        audioRecorder.meteringEnabled = true
-        audioRecorder.prepareToRecord()
-        audioRecorder.record()
+        try! self.audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
+        self.audioRecorder.delegate = self
+        self.audioRecorder.meteringEnabled = true
+        self.audioRecorder.prepareToRecord()
+        self.audioRecorder.record()
 
+    }
+    
+    private func changeState(state: RecordingState) {
+        
+        switch state {
+        case .Initial:
+            //Initialize the UI
+            self.stopButton.hidden = true
+            self.pauseButton.hidden = true
+            self.resumeButton.hidden = true
+            self.recordingLabel.text = self.TapInstructionString
+            self.recordButton.enabled = true
+        case .Recording:
+            //In the process of Recording
+            self.recordButton.enabled = false
+        case .Record:
+            //Record has started
+            self.recordingLabel.text = self.RecordingString
+            self.stopButton.hidden = false
+            self.pauseButton.hidden = false
+            self.resumeButton.hidden = false
+            //Enable buttons properly
+            self.pauseButton.enabled = true
+            self.resumeButton.enabled = false
+        case .Pausing:
+            //In the process of pausing
+            self.pauseButton.enabled = false
+        case .Paused:
+            //Record is paused
+            self.recordingLabel.text = self.PauseString
+            self.resumeButton.enabled = true
+        case .Stopping:
+            //In the process of stopping the recording
+            self.stopButton.hidden = true
+            self.pauseButton.hidden = true
+            self.resumeButton.hidden = true
+        default:
+            print("Unhandled case for RecordingState")
+        }
+        
+        self.state = state
+        
     }
 
 }
